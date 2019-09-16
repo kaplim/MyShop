5c79747e93b428e62ea98488e0c78c0aa374c6b9
Kap Lim <freekaplim@gmail.com>
2019-09-15 20:43:09 -0400

    Added EntityFramework

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        // Look for <connectionStrings><add name="DefaultConnection" ...
+        // in Myshop.WebUI\Web.config or
+        // in MyShop.DataAccess.SQL\App.config
+        public DataContext()
+            : base("DefaultConnection") 
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
